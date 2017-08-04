ea75a74a35bdea22826621d8a67715b43d98f69f
Your name <you@example.com>
2017-08-04 23:45:34 +0900

    first commmit

---
+++ views.py
@@ -0,0 +1,42 @@
+#-*- coding: utf-8 -*-
+
+from datetime import datetime
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0')
