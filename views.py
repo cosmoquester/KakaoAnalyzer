59307b0c0a6f47bdc34c14591ef03f4c4eefe748
ParkSangJun <ParkSangJun@PSJLAPTOP>
2017-08-05 10:11:07 +0900

    little

--- views.py
+++ views.py
@@ -23,7 +23,7 @@ def contact():
         'contact.html',
         title='Contact',
         year=datetime.now().year,
-        message='Your contact page.'
+        message='Hello!'
     )
 
 @app.route('/about')
@@ -33,7 +33,7 @@ def about():
         'about.html',
         title='About',
         year=datetime.now().year,
-        message='Your application description page.'
+        message='Description & Usage'
     )
 
 
