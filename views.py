e8af1b24901be6c458cfe8c6fd4e7717cd6064c2
Your name <you@example.com>
2017-08-05 09:55:28 +0900

    go

--- views.py
+++ views.py
@@ -12,7 +12,7 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Home Page',
+        title='KakaoTalk Analizer',
         year=datetime.now().year,
     )
 
