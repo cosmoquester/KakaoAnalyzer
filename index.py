317d7f308394e5ce5ef9436759118ee488168485
Your name <you@example.com>
2017-08-06 15:24:09 +0900

    chart added

--- index.py
+++ index.py
@@ -52,14 +52,27 @@ def Analizing():
     analedline, people=analizer.linechk(request.form["data"])
 #    for i in people.keys():
 #	print(people[i].name+" "+str(people[i].ynum*1.0/people[i].cnum))
+    names=sorted(people.keys(),key=lambda x:people[x].cnum, reverse=True)
+    ynames=sorted(people.keys(), key=lambda x:people[x].ynum*1.0/people[x].cnum, reverse=True)
+    values = []
+    for i in names:
+	values.append(people[i].cnum)
+    values_y = []
+    for i in ynames:
+	values_y.append(people[i].ynum)
+    colors = [ "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#DDDDDD", "#ABCABC", "#FE2EC8 ", "#01DF01 ", "#B43104 ", "#FA8258 ", "#D8F781 ", "#642EFE ", "#58FAAC ", "#00FF00 ", "#0B4C5F" ]
+    while len(colors)<len(names):
+	colors.append('#000000')
     return render_template(
 	'result_cnum.html', 
 	title='Result', 
 	year=datetime.now().year,
 	people=people,
 	message="Thanks",
-	names=sorted(people.keys(),key=lambda x:people[x].cnum, reverse=True),
-	ynames=sorted(people.keys(), key=lambda x:people[x].ynum*1.0/people[x].cnum, reverse=True),
+	names=names,
+	ynames=ynames,
+	set=zip(values, names, colors),
+	set_y=zip(values_y, ynames, colors),
     )
 
 
