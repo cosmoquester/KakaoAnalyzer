2dbc6d1a3871bb98ecb3e3b45b610b5976a0f809
박상준 <psj8252@naver.com>
2019-07-25 11:23:24 +0900

    Add Mode Option

--- kakaoAnalyzer/Analyzer.py
+++ kakaoAnalyzer/Analyzer.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from .msgstruct import *
 
 
-def Analyze(data_in, line_num=None, line_analyze=None):
+def Analyze(data_in, line_num=None, line_analyze=None, mode=None):
     '''
     Analyze kakaoTalk text. input parameter is file io or string.
     It returns Chatroom instance.
@@ -20,27 +20,44 @@ def Analyze(data_in, line_num=None, line_analyze=None):
         from io import StringIO
         data_in = StringIO(data_in)
 
+    # Select Mode
+    if mode:
+        if mode in ('android', 1):
+            mode = 2 # 1 is Android
+        elif mode in ('winpc', 'win', 2):
+            mode = 1 # 2 is Windows PC
+        elif mode in ('ios', 'ipad', 3):
+            mode = 3 # 3 is iOS
+        else:
+            mode = None
+
     # Find Chatroom Name
     line = data_in.readline()
     chatname = search('(.+?) 님과 카카오톡 대화|(.+?) \d+ 카카오톡 대화', line)
 
-    # Android or PC
+    # Set Chatname
     if chatname:
         chatname = chatname.group(1,2)
-        # Android
-        if chatname[0] == None:
-            chatname = chatname[1]
-            datetime_exp = compile('(?P<year>\d{4})년 (?P<month>\d{1,2})월 (?P<day>\d{1,2})일 .. \d{1,2}:\d{2}\r?\n?$')
-            message_exp = compile('\d{4}년 \d{1,2}월 \d{1,2}일 (?P<afm>..) (?P<hour>\d{1,2}):(?P<min>\d{2}), (?P<name>.+?) : (?P<con>.+)')
-            etc_exp = compile('\d{4}년 \d{1,2}월 \d{1,2}일 .. \d{1,2}:\d{1,2}, .+')
-        # PC
-        else:
-            chatname = chatname[0]
-            datetime_exp = compile('-+ (?P<year>\d{4})년 (?P<month>\d{1,2})월 (?P<day>\d{1,2})일 .요일 -+\r?\n?')
-            message_exp = compile('\[(?P<name>.+?)\] \[(?P<afm>..) (?P<hour>\d{1,2}):(?P<min>\d{2})\] (?P<con>.+)')
-            etc_exp = compile('.+님이 나갔습니다.\r?\n?|.+님이 .+님을 초대하였습니다.\r?\n?|.+님이 들어왔습니다.\r?\n?')
-    # ipad
+        chatname = chatname[0] if chatname[0] else chatname[1]
     else:
+        chatname = line
+
+    # Android
+    if mode == 1:
+        chatname = chatname[1]
+        datetime_exp = compile('(?P<year>\d{4})년 (?P<month>\d{1,2})월 (?P<day>\d{1,2})일 .. \d{1,2}:\d{2}\r?\n?$')
+        message_exp = compile('\d{4}년 \d{1,2}월 \d{1,2}일 (?P<afm>..) (?P<hour>\d{1,2}):(?P<min>\d{2}), (?P<name>.+?) : (?P<con>.+)')
+        etc_exp = compile('\d{4}년 \d{1,2}월 \d{1,2}일 .. \d{1,2}:\d{1,2}, .+')
+
+    # Windows PC
+    elif mode == 2:
+        chatname = chatname[0]
+        datetime_exp = compile('-+ (?P<year>\d{4})년 (?P<month>\d{1,2})월 (?P<day>\d{1,2})일 .요일 -+\r?\n?')
+        message_exp = compile('\[(?P<name>.+?)\] \[(?P<afm>..) (?P<hour>\d{1,2}):(?P<min>\d{2})\] (?P<con>.+)')
+        etc_exp = compile('.+님이 나갔습니다.\r?\n?|.+님이 .+님을 초대하였습니다.\r?\n?|.+님이 들어왔습니다.\r?\n?')
+
+    # ipad
+    elif mode == 3:
         chatname = line
         datetime_exp = compile('(?P<year>\d{4})년 (?P<month>\d{1,2})월 (?P<day>\d{1,2})일 .요일\r?\n?')
         message_exp = compile('(?P<year>\d{4}). (?P<month>\d{1,2}). (?P<day>\d{1,2}). (?P<afm>..) (?P<hour>\d{1,2}):(?P<min>\d{1,2}), (?P<name>.+?) : (?P<con>.+?)\r?\n?$')
